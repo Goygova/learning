771f17216aee07a72e18af75c8ba14f06126c629
Anastasia Goygova <goygova@gmail.com>
2019-07-09 19:26:46 -0700

    Updated form builder

--- main.js
+++ main.js
@@ -3,15 +3,30 @@ function addField(){
   let fieldName = prompt('Enter field name:');
     if(fieldName){
       let inputEl = document.createElement('input');
-      let div = document.createElement('div');
-      div.innerHTML = fieldName + ':';
-      div.appendChild(inputEl);
+
+      let fieldContainer = document.createElement('div');
+      fieldContainer.className = 'field-container';
+
+      let removeButton = document.createElement('button');
+      removeButton.innerHTML = 'Remove field';
+      removeButton.onclick = removeField;
+
+      let fieldNameContainer = document.createElement('div');
+      fieldNameContainer.className = 'field-name-container';
+      fieldNameContainer.innerHTML = fieldName + ':';
+
+      fieldContainer.appendChild(fieldNameContainer);
+      fieldContainer.appendChild(inputEl);
+      fieldContainer.appendChild(removeButton);
+
       let container = document.getElementById('container');
+
       let submitButton = document.getElementById('submit-button');
+
       if(submitButton){
-        container.insertBefore(div, submitButton);
+        container.insertBefore(fieldContainer, submitButton);
       } else {
-        container.appendChild(div);
+        container.appendChild(fieldContainer);
       }
       fieldsCounter++;
   }
@@ -29,4 +44,16 @@ function renderSubmitButton(){
   let container = document.getElementById('container');
   container.appendChild(submitButton);
 }
-//TODO: add remove field button
+
+function removeField(){
+  this.parentElement.parentElement.removeChild(this.parentElement);
+  fieldsCounter--;
+  //if last field is removed then remove submit button
+  if(fieldsCounter === 0){
+    let submitButtonParent = document.getElementById('container');
+    let submitButton = document.getElementById('submit-button');
+    submitButtonParent.removeChild(submitButton);
+  }
+}
+
+
