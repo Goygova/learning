8a94938a171ce58a531c443b7c99329fa0a06fcf
Anastasia Goygova <goygova@gmail.com>
2019-07-10 18:08:07 -0700

    Aded commets to the code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let fieldsCounter = 0;
 const MAX_NUMBER_OF_FIELDS = 3;
 
+/* Function that adds field on the page */
 function addField(){
   let fieldName = prompt('Enter field name:');
   if(doesFieldExist(fieldName)){
@@ -24,6 +25,9 @@ function addField(){
 
     let submitButton = document.getElementById('submit-button');
 
+    /* if submit button exists then add field before button 
+    otherwise add field at the end
+    */
     if(submitButton){
       container.insertBefore(fieldContainer, submitButton);
     } else {
@@ -31,6 +35,7 @@ function addField(){
     }
     fieldsCounter++;
 
+    /* if add button disabled add tooltip on hover with text*/
     if(shouldDisableAddFieldButton()){
       let addFieldButton = document.getElementById('add-field-button');
       addFieldButton.disabled = true;
@@ -38,6 +43,8 @@ function addField(){
       addFieldButton.appendChild(tooltip);
     }
   }
+    /* if amount of fields more than 0 and submit button doesnt exist
+    create submit button and insert it as a child into container */
   if(fieldsCounter > 0){
     if(!document.getElementById('submit-button')){
       let submitButton = createSubmitButton();
@@ -46,20 +53,20 @@ function addField(){
     }
   }
 }
-
+/* creating submit button function*/
 function createSubmitButton(){
   let submitButton = document.createElement('button');
   submitButton.innerHTML = 'Submit';
   submitButton.id = 'submit-button';
   return submitButton;
 }
-
+/* function that removes submit button */
 function removeSubmitButton(){
   let submitButtonParent = document.getElementById('container');
   let submitButton = document.getElementById('submit-button');
   submitButtonParent.removeChild(submitButton);
 }
-
+/* function that creats remove add field button*/
 function createRemoveButton(){
   let removeButton = document.createElement('button');
   removeButton.innerHTML = 'Remove field';
@@ -67,6 +74,7 @@ function createRemoveButton(){
   return removeButton;
 }
 
+/* function that creates field name container with name that provided user */
 function createFieldNameContainer(fieldName){
   let fieldNameContainer = document.createElement('div');
   fieldNameContainer.className = 'field-name-container';
@@ -74,10 +82,12 @@ function createFieldNameContainer(fieldName){
   return fieldNameContainer;
 }
 
+/* function that removes field */
 function removeField(){
   this.parentElement.parentElement.removeChild(this.parentElement);
   fieldsCounter--;
 
+  /* reversing disabled add field button if amount of fields is less than MAX*/
   if(!shouldDisableAddFieldButton()){
     let addFieldButton = document.getElementById('add-field-button');
     addFieldButton.disabled = false;
@@ -88,18 +98,19 @@ function removeField(){
     removeSubmitButton();
   }
 }
-
+/* function that checks if we need to disable add field button */
 function shouldDisableAddFieldButton(){
   return fieldsCounter === MAX_NUMBER_OF_FIELDS;
 }
-
+/* creating tooltip for disabled button add field */
 function createTooltip(){
   let tooltip = document.createElement('span');
   tooltip.className = 'tooltiptext';
   tooltip.innerHTML = `No more than ${MAX_NUMBER_OF_FIELDS} fields can be added`;
   return tooltip;
 }
-
+/* function that checks existing name fields and name from the input 
+for duplicates */
 function doesFieldExist(fieldName){
   let allFieldNames = document.getElementsByClassName('field-name-container');
   for(let i = 0; i < allFieldNames.length; i++){
